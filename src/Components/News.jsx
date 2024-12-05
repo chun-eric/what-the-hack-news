c4a0fe288d22949417f18f37e5a8f43e1c2efd85
Eric Chun <eric.chun88800888@gmail.com>
2024-12-05 21:27:27 +0900

    Refactoring the footer and Trending into separate components and changing it to tailwind css

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,5 +1,6 @@
 import menuData, { dummyNewsData, businessNewsData } from "../data.js";
 import { Avatar } from "@material-tailwind/react";
+import Trending from "../Components/trending/Trending.jsx";
 
 const News = () => {
   const currentYear = new Date().getFullYear();
@@ -25,7 +26,7 @@ const News = () => {
         <h1 className='logo'>What_The_Hack</h1>
 
         <div className='py-0 px-[1rem] flex w-3/4 '>
-          <div className='relative w-full flex justify-center'>
+          <div className='relative flex justify-center w-full'>
             <input
               id='search'
               className=' w-3/4  pl-6 pr-12 h-14 border rounded-full focus:outline-none focus:ring-1 focus:ring-[royalBlack] '
@@ -37,7 +38,7 @@ const News = () => {
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 24 24'
               fill='currentColor'
-              className='w-7 h-7 absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none'
+              className='absolute text-gray-400 -translate-y-1/2 pointer-events-none w-7 h-7 right-4 top-1/2'
             >
               <path
                 fillRule='evenodd'
@@ -63,7 +64,7 @@ const News = () => {
                 <div key={menu.id} className='category'>
                   <span className='icon'>
                     {/* render as an menu icon component when wrapped in < /> */}
-                    <menu.icon className='h-5 w-5' />
+                    <menu.icon className='w-5 h-5' />
                   </span>
                   <span>{menu.category}</span>
                 </div>
@@ -100,75 +101,75 @@ const News = () => {
           {/* business section */}
           <div className='spacer'></div>
           <h1 className='section-heading'>Business</h1>
-          <div className='business-section w-full flex gap-2'>
-            <div className='business-left-section'>
+          <div className='flex w-full  gap-4 h-[500px] mb-6'>
+            <div className='w-[48%] h-[500px] flex flex-col gap-2 mb-5'>
               <img
-                className='business-left-image'
+                className='rounded-[0.6rem] h-[50%]'
                 src={firstBusinessNews.image}
                 alt='business image'
               />
-              <div className='flex gap-2 align-center justify-between items-center '>
-                <div className='flex gap-2 mt-4 items-center'>
+              <div className='flex items-center justify-between gap-2 align-center '>
+                <div className='flex items-center gap-2 mt-4'>
                   <Avatar
                     variant='circular'
                     style={{ width: "40px", height: "40px" }}
                     alt={firstBusinessNews.author}
-                    className='border-2 rounded-full border-white hover:z-10 focus:z-10'
+                    className='border-2 border-white rounded-full hover:z-10 focus:z-10'
                     src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
                   />
                   <p className='text-sm'>{firstBusinessNews.author}</p>
-                  <span className='h-1 w-1 bg-gray-400 rounded-full inline-block'></span>
+                  <span className='inline-block w-1 h-1 bg-gray-400 rounded-full'></span>
                   <p className='text-sm text-gray-800 '>
                     {firstBusinessNews.category}
                   </p>
                 </div>
                 <div className='flex items-center mt-4'>
-                  <p className='text-xs text-gray-400 items-center'>
+                  <p className='items-center text-xs text-gray-400'>
                     {firstBusinessNews.dateAdded}
                   </p>
                 </div>
               </div>
-              <h3 className='mt-5 font-bold text-black text-lg'>
+              <h3 className='mt-5 text-lg font-bold text-black'>
                 {firstBusinessNews.title}
               </h3>
-              <p className='line-clamp-2 text-sm text-gray-400 pt-2'>
+              <p className='pt-2 text-sm text-gray-400 line-clamp-2'>
                 {firstBusinessNews.snippet}
               </p>
             </div>
             {/*** right section */}
-            <div className='business-right-section space-x-2 '>
+            <div className='space-x-2 w-[50%] h-[500px]  '>
               {businessNewsData.slice(1).map((newsItem) => (
                 <div
                   key={newsItem.id}
-                  className='flex flex-col w-full mb-4  pb-4 border-b '
+                  className='flex flex-col w-full pb-4 mb-4 border-b h-1/3 last:border-b-0 '
                 >
-                  <div className='flex flex-row gap-4 align-center'>
+                  <div className='flex flex-row items-center gap-4'>
                     <img
-                      className='w-1/3 rounded-[0.6rem] object-cover'
+                      className='w-1/3 h-24 rounded-[0.6rem] object-cover'
                       src={newsItem.image}
                       alt='business image'
                     />
-                    <h3 className='w-2/3 mt-5 font-bold text-black text-lg'>
+                    <h3 className='w-2/3 mt-5 text-lg font-bold text-black '>
                       {newsItem.title}
                     </h3>
                   </div>
-                  <div className='flex gap-2 align-center justify-between items-center  '>
-                    <div className='flex gap-2 mt-4 items-center '>
+                  <div className='flex items-center justify-between gap-2 align-center '>
+                    <div className='flex items-center gap-2 mt-4 '>
                       <Avatar
                         variant='circular'
                         style={{ width: "40px", height: "40px" }}
                         alt={newsItem.author}
-                        className='border-2 rounded-full border-white hover:z-10 focus:z-10'
+                        className='border-2 border-white rounded-full hover:z-10 focus:z-10'
                         src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
                       />
                       <p className='text-sm'>{newsItem.author}</p>
-                      <span className='h-1 w-1 bg-stone-950 rounded-full inline-block'></span>
+                      <span className='inline-block w-1 h-1 rounded-full bg-stone-950'></span>
                       <p className='text-sm text-gray-800 '>
                         {newsItem.category}
                       </p>
                     </div>
                     <div className='flex items-center mt-4'>
-                      <p className='text-xs text-gray-400 items-center'>
+                      <p className='items-center text-xs text-gray-400'>
                         {newsItem.dateAdded}
                       </p>
                     </div>
@@ -180,15 +181,12 @@ const News = () => {
         </div>
 
         {/* Trending section */}
-        <div className='trending-section'>
-          <div className=''>
-            <h1 className='trending-heading'>Latest </h1>
-          </div>
-        </div>
+        <Trending />
 
-        <footer>
-          <p className='copyright'>What_The_Hack</p>
-          <p className=''>
+        {/* Footer */}
+        <footer className='w-full h-full p-8 max-h-28 bg-[#eeeeee] flex items-center justify-center z-[4] flex-col font-["Bebas_Neue"] absolute bottom-0 left-0 rounded-b-[0.6rem]'>
+          <p className='text-xl font-["Bebas_Neue"] '>What_The_Hack</p>
+          <p className='text-xl font-["Bebas_Neue"]'>
             &copy; {currentYear} All Rights Reserved. Created by Eric Chun
           </p>
         </footer>
---
+++ src/Components/trending/Trending.jsx
@@ -0,0 +1,13 @@
+const Trending = () => {
+  return (
+    <div className='w-[12%] h-full px-7 flex flex-col gap-8 text-[#111214] border-l border-[#dddddd]'>
+      <div className=''>
+        <h1 className='text-2xl font-["Bebas_Neue"] font-medium tracking-[0.1rem] self-start'>
+          Latest{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
