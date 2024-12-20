1a36d30c1a844d027c7f5279647d34075b4554a8
Eric Chun <eric.chun88800888@gmail.com>
2024-12-20 10:20:05 +0900

    Added analytics to web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import News from "./Components/News";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <div className='w-full h-screen grid place-items-center'>
+    <div className='grid w-full h-screen place-items-center'>
       <News />
+      <Analytics />
     </div>
   );
 };
